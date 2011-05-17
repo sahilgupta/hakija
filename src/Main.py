2f70345cccdc0b25fef32a2fc9c657fb47aa5f36
Sahil Gupta <sahilgupta29@gmail.com>
2011-05-17 18:42:43 +0530

    Worked around the weekend data problem. Fetches all files irrespective of day. Useful incase of Special Mahurat sessions

--- src/Main.py
+++ src/Main.py
@@ -16,39 +16,44 @@ br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('Host','www.nseindia.com'),('User-Agent',' Mozilla/5.0 (X11; Linux i686; rv:2.0) Gecko/20100101 Firefox/4.0'),('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),('Accept-Language',' en-us,en;q=0.5'),('Accept-Charset',' ISO-8859-1,utf-8;q=0.7,*;q=0.7'),('Keep-Alive','115'),('Connection',' keep-alive')]
 
-startdate=date(2011,04,25)
-enddate=date(2011,04,29)
+startdate=date(2011,05,01)
+enddate=date(2011,05,16)
 d = startdate
 delta = datetime.timedelta(days=1)
 
 while d <= enddate:
-    if(d.isoweekday() in range(1,6)):
+#    if(d.isoweekday() in range(1,6)):
         #Continue only if the day is a weekday!
+        flag=0
         date = d.strftime("%d-%m-%Y")
         res = br.open("http://www.nseindia.com/archives/archives.jsp?date="+date+"&fileType=eqbhav")
         data = res.read()
         for link in br.links():
             rlink = "http://nseindia.com"+link.url
             urllib.urlretrieve(rlink,date+".zip")
+            flag=1
             print "File fetched!"
             break
-        call(["unzip", "-oq", os.getcwd()+"/"+date+".zip"])
-        call(["rm", os.getcwd()+"/"+date+".zip"])
         
-        files=os.listdir(".")
-        files=[filename for filename in files if filename[-4:] == '.csv']
-    
-        f = open(os.getcwd()+"/"+files[0], 'r')
-        x = f.readlines()
-        f.close()
+        if(flag):
+            call(["unzip", "-oq", os.getcwd()+"/"+date+".zip"])
+            call(["rm", os.getcwd()+"/"+date+".zip"])
         
-        nfile= d.strftime("%d-%m-%Y") + ".txt"
-        print nfile
-        f = open(nfile, 'w')
-        for x1 in x[1:]:
-            x1 = x1.split(',')
-            f.write(x1[0] + "," + d.strftime("%Y%m%d") + "," + x1[2] + "," + x1[3] + "," + x1[4] + "," + x1[5] + "," + x1[8] + "\n")
-        f.close()
-        call(["rm", os.getcwd()+"/"+files[0]])
+            files=os.listdir(".")
+            files=[filename for filename in files if filename[-4:] == '.csv']
+            
+            if len(files):
+                f = open(os.getcwd()+"/"+files[0], 'r')
+                x = f.readlines()
+                f.close()
+                
+                nfile= d.strftime("%d-%m-%Y") + ".txt"
+                print nfile
+                f = open(nfile, 'w')
+                for x1 in x[1:]:
+                    x1 = x1.split(',')
+                    f.write(x1[0] + "," + d.strftime("%Y%m%d") + "," + x1[2] + "," + x1[3] + "," + x1[4] + "," + x1[5] + "," + x1[8] + "\n")
+                f.close()
+                call(["rm", os.getcwd()+"/"+files[0]])
         time.sleep(2)
-    d += delta
+        d += delta
